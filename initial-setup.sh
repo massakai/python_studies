50e5134614b63920fd705db3799978bb3dee9ecf
Masashi SAKAI <masashi.sakai1986@gmail.com>
2014-08-13 19:32:56 +0900

    add init script

---
+++ initial-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo aptitude -y update
+sudo aptitude -y upgrade
+
+sudo aptitude -y install build-essential \
+                         libsqlite3-dev \
+                         libreadline6-dev \
+                         libgdbm-dev \
+                         zlib1g-dev \
+                         libbz2-dev \
+                         sqlite3 \
+                         tk-dev \
+                         zip \
+                         python-dev
+
+# install distribute
+sudo chmod -R 0775 /usr/local
+sudo chgrp -R bpbook /usr/local
+wget http://python-distribute.org/distribute_setup.py
+sudo python distribute_setup.py
+
+# install pip
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+sudo python get-pip.py
+
+# install virtualenv
+sudo pip install virtualenv virtualenvwrapper
