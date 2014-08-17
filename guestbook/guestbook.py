524d4255aeabd5c5f89baee6a34afac0602aa2f1
Masashi SAKAI <masashi.sakai1986@gmail.com>
2014-08-17 15:15:07 +0000

    can save comments

---
+++ guestbook/guestbook.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import shelve
+
+DATA_FILE = 'guestbook.dat'
+
+def save_data(name, comment, create_at):
+    u"""投稿データを保存します
+    """
+    # shelveモジュールでデータベースファイルを開きます
+    database = shelve.open(DATA_FILE)
+    # データベースにgreeting_listがなければ、新しくリストを作ります
+    if 'greeting_list' not in database:
+        greeting_list = []
+    else:
+        # データベースからデータを取得します
+        greeting_list = database['greeting_list']
+    # リストの先頭に投稿データを追加します
+    greeting_list.insert(0, {
+        'name': name,
+        'comment': comment,
+        'create_at': create_at,
+    })
+    # データベースを更新します
+    database['greeting_list'] = greeting_list
+    # データベースファイルを閉じます
+    database.close()
